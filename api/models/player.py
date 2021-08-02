41e91bbbe54848d95b6819ee797ddf0b9b4582ad
Skelmis <skelmis.craft@gmail.com>
2021-08-02 21:56:12 +1200

    Add models

--- api/models/player.py
+++ api/models/player.py
@@ -6,3 +6,6 @@ from .event import Event
 class Player(models.Model):
     event_pk = models.ForeignKey(Event, on_delete=models.CASCADE)
     username = models.TextField()
+
+    class Meta:
+        unique_together = ["event_pk", "username"]
