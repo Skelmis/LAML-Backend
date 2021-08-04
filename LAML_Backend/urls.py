ac05ee15b638144a955b85c00902932f89651e00
skelmis <skelmis.craft@gmail.com>
2021-08-04 23:27:11 +1200

    Addd documentation

--- LAML_Backend/urls.py
+++ LAML_Backend/urls.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Stats API",
+        default_version="v1",
+        description="A basic API backend for Localized Stats",
+        terms_of_service="https://koldfusion.xyz/",
+        contact=openapi.Contact(email="ethan@koldfusion.xyz"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(
+        r"^doc(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path("docs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
     path("", include("api.urls")),
 ]
