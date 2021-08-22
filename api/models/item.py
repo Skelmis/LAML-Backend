37893efedec7d34160c18c426fc7d40a9c1dd6b4
skelmis <skelmis.craft@gmail.com>
2021-08-22 21:10:59 +1200

    Change to floats

--- api/models/item.py
+++ api/models/item.py
@@ -5,5 +5,5 @@ from .player import Player
 
 class Item(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE, editable=False)
-    amount = models.PositiveSmallIntegerField()
+    amount = models.FloatField()
     time_created = models.DateTimeField(auto_now_add=True, editable=False)
