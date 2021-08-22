8db63e09ece3930f4c27bfe3fe48fb66f58ea92a
skelmis <skelmis.craft@gmail.com>
2021-08-22 21:45:20 +1200

    CORS Stuff

--- LAML_Backend/settings.py
+++ LAML_Backend/settings.py
@@ -20,7 +20,10 @@ if str(os_debug) == "1":
 else:
     DEBUG = False
 
-# ALLOWED_HOSTS = ["leaderboard.koldfusion.xyz", "127.0.0.1", "*"]
+# ALLOWED_HOSTS = ["leaderboard.koldfusion.xyz", "127.0.0.1"]
+# CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_WHITELIST = ("http://localhost:3000", "app.koldfusion.xyz")
+
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
