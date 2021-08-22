5dd12166b5ed0c0f48b00f1b6d503a2ebafb20e1
skelmis <skelmis.craft@gmail.com>
2021-08-22 21:59:54 +1200

    Change database back

--- LAML_Backend/settings.py
+++ LAML_Backend/settings.py
@@ -83,7 +83,6 @@ WSGI_APPLICATION = "LAML_Backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -99,7 +98,7 @@ DATABASES = {
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
