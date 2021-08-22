447d58e9388362c8fba441755cbef5625b064002
skelmis <skelmis.craft@gmail.com>
2021-08-22 21:12:04 +1200

    Fix routing

--- LAML_Backend/urls.py
+++ LAML_Backend/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path("api/auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("", include("base.urls")),
+    path("", include("api.urls")),
+    # path("", include("base.urls")),
 ]
