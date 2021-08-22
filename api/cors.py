384e233ac44f2884c38a1df372df7fe3d9b5a328
skelmis <skelmis.craft@gmail.com>
2021-08-22 22:20:03 +1200

    Modify cors middleware

--- api/cors.py
+++ api/cors.py
@@ -1,4 +1,7 @@
 class CorsMiddleware:
+    def __init__(self, *args, **kwargs):
+        pass
+
     def process_response(self, req, resp):
         resp["Access-Control-Allow-Origin"] = "*"
         return resp
