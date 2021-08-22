c6e5e0dbf04e72b07969d182b8539daf01ad9623
skelmis <skelmis.craft@gmail.com>
2021-08-23 04:32:03 +1200

    Revert database

--- LAML_Backend/settings.py
+++ LAML_Backend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "LAML_Backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -102,7 +102,7 @@ DATABASES = {
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
